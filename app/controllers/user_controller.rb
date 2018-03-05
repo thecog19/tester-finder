535f7e1489c98d91295f0c441ed5284c45639ab9
felipesuero <felipe@motivislearning.com>
2018-03-05 15:31:52 -0500

    finished sorting by bugs and getting bug counts

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,6 +3,11 @@ class UserController < ApplicationController
 	def index
 		@device = Device.all
 		@countries = User.select(:country).map(&:country).uniq
+		if(params[:search])
+			@users = User.search(params[:search])
+		else
+			@users = User.order_by_bugcount
+		end
 	end
 
 	def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,19 @@ class User < ApplicationRecord
 	 validates :external_id, uniqueness: true, presence: true 
 	 validates :country, length: {is: 2}
 	 has_and_belongs_to_many :devices, join_table: :tester_devices, foreign_key: 'user_id', primary_key: 'device_id'
-	 has_many :bugs, through: :bugs, primary_key: "user_id"
+	 has_many :bugs, foreign_key: "user_id"
+
+	 def self.order_by_bugcount
+	 	User.left_joins(:bugs).group(:user_id).order('COUNT(bugs.id) DESC')
+	 end
+
+	 def self.search(search_terms)
+	 	country = search_terms[:country]
+	 	device_id = search_terms[:device]
+	 	user = User.all
+	 	unless(country == "All")
+	 		user = user.where(country: country)
+	 	end
+	 	user = user.joins("JOIN bugs ON users.external_id == user_id").where("device_id == #{device_id}").group(:user_id).order('COUNT(bugs.id) DESC')
+	 end
 end
